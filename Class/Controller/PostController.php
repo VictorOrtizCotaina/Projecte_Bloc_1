b40ee36ea34b89492ab8cf709b02794b9c7fd4f0
VictorOrtizCotaina <v.ortiz.cf@gmail.com>
2019-12-13 12:54:18 -0500

    Projecte.

--- Class/Controller/PostController.php
+++ Class/Controller/PostController.php
@@ -1,2 +1,37 @@
 <?php
+namespace App\Controller;
 
+use App\Model\CategoryModel;
+use App\Model\PostModel;
+use App\Model\TopicModel;
+use App\Model\UserModel;
+
+
+class PostController extends AbstractController
+{
+
+    public function getAdminPost()
+    {
+    }
+
+
+    public function adminPostAdd()
+    {
+    }
+
+
+    public function adminPostEdit($id_post)
+    {
+    }
+
+
+    public function adminPostDelete($id_post)
+    {
+    }
+
+
+    public function adminPostActive($id_post)
+    {
+    }
+
+}
