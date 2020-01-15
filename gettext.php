9d884360b9e13f396974a225846bc4c41895ae29
VictorOrtizCotaina <v.ortiz.cf@gmail.com>
2020-01-15 10:45:52 -0500

    Projecte.

---
+++ gettext.php
@@ -0,0 +1,30 @@
+<?php
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require('../config/bootstrap.php');
+
+
+$tplDir = dirname(__FILE__).'/../templates';
+$tmpDir = dirname(__FILE__).'/../templates.php';
+$loader = new FilesystemLoader($tplDir);
+
+// force auto-reload to always have the latest version of the template
+$twig = new Environment($loader, array(
+    'cache' => $tmpDir,
+    'auto_reload' => true
+));
+$twig->addExtension(new Twig_Extensions_Extension_I18n());
+$twig->addExtension(new Twig_Extensions_Extension_Intl());
+$twig->addExtension(new \Twig\Extra\Markdown\MarkdownExtension());
+
+// configure Twig the way you want
+
+// iterate over all your templates
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($tplDir), RecursiveIteratorIterator::LEAVES_ONLY) as $file)
+{
+    // force compilation
+    if ($file->isFile()) {
+        $twig->loadTemplate(str_replace($tplDir.'/', '', $file));
+    }
+}
